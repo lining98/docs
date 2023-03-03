1f119f78c7ddee7f45615e2f3ac44b29f0efc1e2
lining <ln980908@163.com>
2023-03-03 14:47:00 +0800

    编辑导航

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -10,30 +10,78 @@ module.exports = {
   head: headConf,
   // plugins: pluginsConf,
   lastUpdated: true,
+  cleanUrls: true,
+  markdown: {
+    lineNumbers: true,
+  },
   themeConfig: {
     siteTitle: "lemon's blog", //左上角的
     logo: "/img/logo.png", //左上角的logo,注意：它的路径是从public文件夹开始的，所以这里引用的是public/logo.jpg这张图
     nav: navsConf,
     sidebar: sidebarConf,
-    // socialLinks: [{ icon: "github", link: "https://github.com/lining98" }], //右上角的社交标签，支持多种icon，具体可以查询官网，反正没有QQ和微信，放个git差不多意思意思就行了
+    /* 右侧大纲配置 */
+    outline: {
+      level: "deep",
+      label: "本页目录",
+    },
 
     // 最后更新时间
-    lastUpdatedText: "Updated Date",
-    // lastUpdatedText: "更新时间",
-    // autoCreateIssue: true,
+    lastUpdatedText: "上次更新",
 
     // 页脚配置
     footer: {
-      message: "Released under the MIT License.",
-      copyright: "Copyright © 2019-present Evan You",
+      message: "如有转载或 CV 的请标注本站原文地址",
+      copyright: "Copyright © 2023-present lemon/lining",
     },
     docFooter: {
       prev: "上一页",
       next: "下一页",
     },
-  },
-  markdown: {
-    theme: "material-theme-palenight",
-    lineNumbers: true,
+
+    /* Algolia DocSearch 配置 */
+    algolia: {
+      appId: "",
+      apiKey: "",
+      indexName: "search",
+      placeholder: "搜索",
+      translations: {
+        button: {
+          buttonText: "搜索",
+          buttonAriaLabel: "搜索",
+        },
+        modal: {
+          searchBox: {
+            resetButtonTitle: "清除查询条件",
+            resetButtonAriaLabel: "清除查询条件",
+            cancelButtonText: "取消",
+            cancelButtonAriaLabel: "取消",
+          },
+          startScreen: {
+            recentSearchesTitle: "搜索历史",
+            noRecentSearchesText: "没有搜索历史",
+            saveRecentSearchButtonTitle: "保存至搜索历史",
+            removeRecentSearchButtonTitle: "从搜索历史中移除",
+            favoriteSearchesTitle: "收藏",
+            removeFavoriteSearchButtonTitle: "从收藏中移除",
+          },
+          errorScreen: {
+            titleText: "无法获取结果",
+            helpText: "你可能需要检查你的网络连接",
+          },
+          footer: {
+            selectText: "选择",
+            navigateText: "切换",
+            closeText: "关闭",
+            searchByText: "搜索提供者",
+          },
+          noResultsScreen: {
+            noResultsText: "无法找到相关结果",
+            suggestedQueryText: "你可以尝试查询",
+            reportMissingResultsText: "你认为该查询应该有结果？",
+            reportMissingResultsLinkText: "点击反馈",
+          },
+        },
+      },
+    },
   },
 };
--- docs/nav/data.ts
+++ docs/nav/data.ts
@@ -10,10 +10,22 @@ export const NAV_DATA: NavData[] = [
     title: "常用工具",
     items: [
       {
-        icon: "https://caniuse.com/img/favicon-128.png",
-        title: "Can I use",
-        desc: "前端 API 兼容性查询",
-        link: "https://caniuse.com",
+        icon: "https://static.runoob.com/images/c-runoob-logo.ico",
+        title: "菜鸟工具",
+        desc: "",
+        link: "https://c.runoob.com/",
+      },
+      {
+        icon: "https://shared-https.ydstatic.com/images/favicon.ico",
+        title: "有道翻译",
+        desc: "",
+        link: "https://fanyi.youdao.com/index.html#/",
+      },
+      {
+        icon: "https://unbug.github.io/codelf/images/codelf_logo.f4ae25bd.png",
+        title: "CODEELF",
+        desc: "变量取名",
+        link: "https://unbug.github.io/codelf/",
       },
       {
         icon: "https://tinypng.com/images/apple-touch-icon.png",
@@ -27,11 +39,55 @@ export const NAV_DATA: NavData[] = [
         desc: "开发人员的工具箱",
         link: "https://tool.lu",
       },
+    ],
+  },
+  {
+    title: "社区",
+    items: [
       {
-        icon: "/icons/json-cn.ico",
-        title: "Json 中文网",
-        desc: "JSON 在线解析及格式化验证",
-        link: "https://www.json.cn",
+        title: "Github",
+        icon: {
+          svg: '<svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>GitHub</title><path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"/></svg>',
+        },
+        desc: "一个面向开源及私有软件项目的托管平台",
+        link: "https://github.com",
+      },
+      {
+        title: "码云",
+        icon: "https://gitee.com/favicon.ico",
+        desc: "Gitee 是由开源中国出品的代码托管平台，汇聚了国内优秀的开源项目和作者",
+        link: "https://gitee.com/",
+      },
+      {
+        title: "稀土掘金",
+        icon: "https://lf3-cdn-tos.bytescm.com/obj/static/xitu_juejin_web//static/favicons/apple-touch-icon.png",
+        desc: "面向全球中文开发者的技术内容分享与交流平台",
+        link: "https://juejin.cn",
+      },
+      {
+        title: "知乎",
+        icon: "https://static.zhihu.com/heifetz/assets/apple-touch-icon-60.362a8eac.png",
+        desc: "中文互联网高质量的问答社区和创作者聚集的原创内容平台",
+        link: "https://juejin.cn",
+      },
+      {
+        title: "SegmentFault 思否",
+        icon: "https://static.segmentfault.com/main_site_next/0dc4bace/touch-icon.png",
+        desc: "技术问答开发者社区",
+        link: "https://segmentfault.com",
+      },
+      {
+        title: "博客园",
+        // icon: 'https://common.cnblogs.com/favicon.ico',
+        icon: "/icons/cnblogs.svg",
+        desc: "博客园是一个面向开发者的知识分享社区",
+        link: "https://www.cnblogs.com",
+      },
+      {
+        icon: "https://cdn.sstatic.net/Sites/stackoverflow/Img/apple-touch-icon.png?v=c78bd457575a",
+        title: "Stack Overflow",
+        desc: "全球最大的技术问答网站",
+        link: "https://stackoverflow.com",
       },
     ],
   },
@@ -52,16 +108,22 @@ export const NAV_DATA: NavData[] = [
         link: "https://v2.cn.vuejs.org",
       },
       {
-        icon: "https://cn.vuejs.org/logo.svg",
-        title: "Vue Router",
-        desc: "Vue.js 的官方路由\n为 Vue.js 提供富有表现力、可配置的、方便的路由",
-        link: "https://router.vuejs.org/zh",
+        icon: "https://static.vue-js.com/6280b990-ff19-11ea-85f6-6fac77c0c9b3.png",
+        title: "Vue3 One Piece",
+        desc: "更快，更轻，易维护，更多的原生支持",
+        link: "https://vue3js.cn/",
       },
       {
         icon: "https://pinia.vuejs.org/logo.svg",
         title: "Pinia",
         desc: "符合直觉的 Vue.js 状态管理库",
         link: "https://pinia.vuejs.org/zh",
+      },
+      {
+        icon: "https://cn.vuejs.org/logo.svg",
+        title: "Vuex",
+        desc: "Vuex 是一个专为 Vue.js 应用程序开发的状态管理模式 + 库。",
+        link: "https://vuex.vuejs.org/zh/",
       },
       {
         icon: "https://nuxt.com/icon.png",
@@ -74,6 +136,12 @@ export const NAV_DATA: NavData[] = [
         title: "VueUse",
         desc: "Vue Composition API 的常用工具集",
         link: "https://vueuse.org",
+      },
+      {
+        icon: "https://web-assets.dcloud.net.cn/unidoc/zh/icon.png?v=1556263038788",
+        title: "uni-app",
+        desc: "uni-app 是一个使用 Vue.js 开发所有前端应用的框架，开发者编写一套代码，可发布到iOS、Android、Web（响应式）、以及各种小程序（微信/支付宝/百度/头条/飞书/QQ/快手/钉钉/淘宝）、快应用等多个平台。",
+        link: "https://uniapp.dcloud.net.cn/",
       },
       {
         icon: "https://element-plus.org/images/element-plus-logo-small.svg",
@@ -146,24 +214,24 @@ export const NAV_DATA: NavData[] = [
         desc: "构建移动 WEB 应用程序的 React 组件库",
         link: "https://mobile.ant.design",
       },
-      {
-        icon: "https://docs.pmnd.rs/apple-touch-icon.png",
-        title: "Zustand",
-        desc: "一个小型、快速、可扩展的 React 状态管理解决方案",
-        link: "https://docs.pmnd.rs/zustand/getting-started/introduction",
-      },
-      {
-        icon: "https://valtio.pmnd.rs/favicon.ico",
-        title: "Valtio",
-        desc: "makes proxy-state simple for React and Vanilla",
-        link: "https://valtio.pmnd.rs",
-      },
-      {
-        icon: "https://jotai.org/favicon.svg",
-        title: "Jotai",
-        desc: "primitive and flexible state management for React",
-        link: "https://jotai.org",
-      },
+      // {
+      //   icon: "https://docs.pmnd.rs/apple-touch-icon.png",
+      //   title: "Zustand",
+      //   desc: "一个小型、快速、可扩展的 React 状态管理解决方案",
+      //   link: "https://docs.pmnd.rs/zustand/getting-started/introduction",
+      // },
+      // {
+      //   icon: "https://valtio.pmnd.rs/favicon.ico",
+      //   title: "Valtio",
+      //   desc: "makes proxy-state simple for React and Vanilla",
+      //   link: "https://valtio.pmnd.rs",
+      // },
+      // {
+      //   icon: "https://jotai.org/favicon.svg",
+      //   title: "Jotai",
+      //   desc: "primitive and flexible state management for React",
+      //   link: "https://jotai.org",
+      // },
       {
         icon: "https://cn.redux.js.org/img/redux.svg",
         title: "Redux",
@@ -185,32 +253,133 @@ export const NAV_DATA: NavData[] = [
     ],
   },
   {
-    title: "JavaScript 框架类库",
+    title: "类库",
     items: [
+      {
+        icon: "http://www.axios-js.com/icon/apple-touch-icon-114x114.png",
+        title: "axios中文文档",
+        desc: "易用、简洁且高效的http库",
+        link: "http://www.axios-js.com/zh-cn/docs/",
+      },
+      {
+        icon: "https://www.lodashjs.com/img/favicon.ico",
+        title: "Lodash 中文文档",
+        desc: "Lodash 是一个一致性、模块化、高性能的 JavaScript 实用工具库。",
+        link: "https://github.com/cdoco/learn-regex-zh",
+      },
       // {
-      //   icon: "https://svelte.dev/svelte-logo-horizontal.svg",
-      //   title: "Svelte",
-      //   desc: "将声明性组件转换为精准高效更新 DOM 的 JavaScript 代码",
-      //   link: "https://svelte.dev",
+      //   icon: "",
+      //   title:"jquery",
+      //   desc: "一个兼容多浏览器的JavaScript框架",
+      //   link: "",
       // },
-      // {
-      //   // icon: 'https://simpleicons.org/icons/jquery.svg',
-      //   icon: "/icons/jquery.svg",
-      //   title: "jQuery API 中文文档",
-      //   desc: "一个兼容多浏览器的 JavaScript 框架",
-      //   link: "https://jquery.cuishifeng.cn",
-      // },
+      {
+        icon: "http://momentjs.cn/static/img/moment-favicon.png",
+        title: "Monment",
+        desc: "JavaScript 日期处理类库",
+        link: "http://momentjs.cn/",
+      },
+      {
+        icon: "https://dayjs.fenxianglu.cn/assets/favicon.png",
+        title: "Day.js中文网",
+        desc: "Day.js是一个极简的JavaScript库，可以为现代浏览器解析、验证、操作和显示日期和时间。",
+        link: "https://dayjs.fenxianglu.cn/",
+      },
+      {
+        icon: "http://mockjs.com/assets/img/logo-2.svg",
+        title: "mockjs",
+        desc: "生成随机数据，拦截Ajax请求",
+        link: "http://mockjs.com/",
+      },
+      {
+        icon: "https://www.swiper.com.cn/templets/default/images/logo.png",
+        title: "swiper",
+        desc: "开源、免费、强大的触摸滑动插件",
+        link: "https://www.swiper.com.cn/",
+      },
+      {
+        icon: "https://v5.bootcss.com/docs/5.3/assets/img/favicons/apple-touch-icon.png",
+        title: "bootstrap",
+        desc: "强大、可扩展、功能丰富的前端开发工具包。它提供的功能包括：基于 Sass 进行编译和定制、内置的网格系统和组件、强大的 JavaScript 插件等。",
+        link: "https://v5.bootcss.com/",
+      },
+    ],
+  },
+  {
+    title: "JavaScript学习",
+    items: [
+      {
+        icon: "https://zh.javascript.info/img/favicon/apple-touch-icon-precomposed.png",
+        title: "现代 JavaScript 教程",
+        desc: "以最新的 JavaScript 标准为基准。通过简单但足够详细的内容，为你讲解从基础到高阶的 JavaScript 相关知识。",
+        link: "https://zh.javascript.info/",
+      },
+      {
+        icon: "https://es6.ruanyifeng.com/favicon.ico",
+        title: "ECMAScript 6 入门",
+        desc: "《ECMAScript 6 入门教程》是一本开源的 JavaScript 语言教程，全面介绍 ECMAScript 6 新引入的语法特性。",
+        link: "https://es6.ruanyifeng.com/",
+      },
+      {
+        icon: "https://github.com/fluidicon.png",
+        title: "stephentian/33-js-concepts",
+        desc: "JavaScript开发者应懂的33个概念",
+        link: "https://github.com/stephentian/33-js-concepts",
+      },
+      {
+        icon: "https://github.com/fluidicon.png",
+        title: "lydiahallie/javascript-questions",
+        desc: "JavaScript的问题",
+        link: "https://github.com/lydiahallie/javascript-questions",
+      },
+      {
+        icon: "https://github.com/fluidicon.png",
+        title: "denysdovhan/wtfjs",
+        desc: "一个有趣和棘手的 JavaScript 示例列表。",
+        link: "https://github.com/denysdovhan/wtfjs/blob/master/README-zh-cn.md",
+      },
+      {
+        icon: "https://regexr-cn.com/assets/icons/favicon-16x16.png?1",
+        title: "RegExr:学习、构建和测试 正则表达式",
+        desc: "RegExr 是一个集 学习、构建和测试正则表达式于一身的在线工具。",
+        link: "https://regexr-cn.com/",
+      },
+      {
+        icon: "https://cdn.codejiaonang.com/favicon.ico",
+        title: "编程胶囊-正则表达式入门",
+        desc: "半小时入门，手机也能写正则。全网最好的正则表达式课程不服来战！",
+        link: "https://codejiaonang.com/#/course/regex_chapter1/0/0",
+      },
+      // https://github.com/sorrycc/awesome-javascript
     ],
   },
   {
     title: "CSS 相关",
     items: [
-      // {
-      // icon: "https://postcss.org/assets/logo-3e39b0aa.svg",
-      // title: "PostCSS",
-      // desc: "一个用 JavaScript 转换 CSS 的工具",
-      // link: "https://postcss.org",
-      // },
+      {
+        icon: "https://lhammer.cn/You-need-to-know-css/static/favicon.ico",
+        title: "CSS Tricks",
+        desc: "Web开发者应该掌握的CSS诀窍",
+        link: "https://lhammer.cn/You-need-to-know-css/#/zh-cn/",
+      },
+      {
+        icon: "https://animista.net/safari-pinned-tab.svg",
+        title: "Animista - CSS",
+        desc: "css的动画效果",
+        link: "https://animista.net/",
+      },
+      {
+        icon: "",
+        title: "Hover.css",
+        desc: "鼠标悬停效果",
+        link: "https://ianlunn.github.io/Hover/",
+      },
+      {
+        icon: "https://chokcoco.github.io/CSS-Inspiration/logo2.png",
+        title: "CSS Inspiration",
+        desc: "CSS灵感",
+        link: "https://chokcoco.github.io/CSS-Inspiration/#/",
+      },
       {
         icon: "https://sass-lang.com/assets/img/logos/logo-b6e1ef6e.svg",
         title: "Sass",
@@ -221,7 +390,48 @@ export const NAV_DATA: NavData[] = [
         icon: "https://www.tailwindcss.cn/apple-touch-icon.png",
         title: "TailwindCSS 中文网",
         desc: "一个功能类优先的 CSS 框架",
-        link: "https://www.tailwindcss.cn",
+        link: "https://www.tailwindcss.cn/docs",
+      },
+      {
+        icon: "https://angrytools.com/include/img/apple-touch-icon.png",
+        title: "CSS Animation Kit",
+        desc: "",
+        link: "https://angrytools.com/css/animation/",
+      },
+    ],
+  },
+  {
+    title: "可视化图表库",
+    items: [
+      {
+        icon: "https://echarts.apache.org/zh/images/favicon.png",
+        title: "Apache ECharts",
+        desc: "一个基于 JavaScript 的开源可视化图表库",
+        link: "https://echarts.apache.org/zh/index.html",
+      },
+      {
+        icon: "http://datav.jiaminghi.com/favicon.ico",
+        title: "DataV",
+        desc: "Vue 大屏数据展示组件库",
+        link: "http://datav.jiaminghi.com/",
+      },
+      {
+        icon: "https://img.alicdn.com/imgextra/i3/O1CN01NQARus1gyEAmmQ9T0_!!6000000004210-0-tps-670-670.jpg",
+        title: "DataV.GeoAtlas地理小工具系列",
+        desc: "获取各省市县的json数据",
+        link: "http://datav.aliyun.com/portal/school/atlas/area_selector",
+      },
+      {
+        icon: "http://webgl3d.cn/img/favicon.ico",
+        title: "Three.js中文网",
+        desc: "Web3D可视化系统课程",
+        link: "http://webgl3d.cn/",
+      },
+      {
+        icon: "https://threejs.org/files/favicon_white.ico",
+        title: "threejs",
+        desc: "",
+        link: "https://threejs.org/",
       },
     ],
   },
@@ -235,10 +445,16 @@ export const NAV_DATA: NavData[] = [
         link: "https://nodejs.org/zh-cn",
       },
       {
+        icon: "https://nodejs.org/static/images/favicons/apple-touch-icon.png",
+        title: "Node.Js中文网",
+        desc: "Node.js 是一个免费的、开源的、跨平台的 JavaScript 运行时环境，允许开发人员在浏览器之外编写命令行工具和服务器端脚本.",
+        link: "http://nodejs.p2hp.com/",
+      },
+      {
         icon: "https://expressjs.com/images/favicon.png",
-        title: "Express",
+        title: "Express中文网",
         desc: "基于 Node.js 平台，快速、开放、极简的 Web 开发框架",
-        link: "https://expressjs.com",
+        link: "https://expressjs.com.cn",
       },
       // {
       //   icon: "https://nodejs.org/static/images/favicons/",
@@ -263,6 +479,12 @@ export const NAV_DATA: NavData[] = [
   {
     title: "编译&构建&打包",
     items: [
+      {
+        icon: "https://static.npmjs.com/58a19602036db1daee0d7863c94673a4.png",
+        title: "npmjs",
+        desc: "世界上最流行的软件生态系统",
+        link: "https://www.npmjs.com/",
+      },
       {
         icon: "https://www.webpackjs.com/icon_180x180.png",
         title: "Webpack 中文网",
@@ -311,10 +533,10 @@ export const NAV_DATA: NavData[] = [
     title: "前端学习资料",
     items: [
       {
-        icon: "https://developer.mozilla.org/apple-touch-icon.6803c6f0.png",
-        title: "MDN | Web 开发者指南",
-        desc: "Mozilla 的开发者平台，提供了大量关于 HTML、CSS 和 JavaScript 的详细文档以及广泛的 Web API 参考资",
-        link: "https://developer.mozilla.org/zh-CN",
+        icon: "https://www.w3school.com.cn/ui2019/logo-32-red.png",
+        title: "w3school",
+        desc: "",
+        link: "https://www.w3school.com.cn/",
       },
       {
         icon: "https://static.runoob.com/images/favicon.ico",
@@ -322,55 +544,34 @@ export const NAV_DATA: NavData[] = [
         desc: "学的不仅是技术，更是梦想！",
         link: "https://www.runoob.com",
       },
+      {
+        icon: "https://www.lingtiku.com/favicon.ico",
+        title: "灵题库-前端题库",
+        desc: "",
+        link: "https://www.lingtiku.com/",
+      },
     ],
   },
   {
-    title: "社区",
+    title: "算法",
     items: [
       {
-        title: "Github",
-        icon: {
-          svg: '<svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>GitHub</title><path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"/></svg>',
-        },
-        desc: "一个面向开源及私有软件项目的托管平台",
-        link: "https://github.com",
+        icon: "https://books.halfrost.com/leetcode/apple-touch-icon-60x60.png",
+        title: "leetcode",
+        desc: "",
+        link: "https://leetcode.cn/",
       },
       {
-        icon: "https://cdn.sstatic.net/Sites/stackoverflow/Img/apple-touch-icon.png?v=c78bd457575a",
-        title: "Stack Overflow",
-        desc: "全球最大的技术问答网站",
-        link: "https://stackoverflow.com",
+        icon: "https://www.eveningwater.com/static/image/icon.jpg",
+        title: "剑指offer算法题",
+        desc: "",
+        link: "https://eveningwater.github.io/to-offer/#/",
       },
       {
-        title: "稀土掘金",
-        icon: "https://lf3-cdn-tos.bytescm.com/obj/static/xitu_juejin_web//static/favicons/apple-touch-icon.png",
-        desc: "面向全球中文开发者的技术内容分享与交流平台",
-        link: "https://juejin.cn",
-      },
-      {
-        title: "V2EX",
-        icon: "https://www.v2ex.com/static/icon-192.png",
-        desc: "一个关于分享和探索的地方",
-        link: "https://www.v2ex.com",
-      },
-      {
-        title: "SegmentFault 思否",
-        icon: "https://static.segmentfault.com/main_site_next/0dc4bace/touch-icon.png",
-        desc: "技术问答开发者社区",
-        link: "https://segmentfault.com",
-      },
-      {
-        title: "博客园",
-        // icon: 'https://common.cnblogs.com/favicon.ico',
-        icon: "/icons/cnblogs.svg",
-        desc: "博客园是一个面向开发者的知识分享社区",
-        link: "https://www.cnblogs.com",
-      },
-      {
-        title: "知乎",
-        icon: "https://static.zhihu.com/heifetz/assets/apple-touch-icon-60.362a8eac.png",
-        desc: "中文互联网高质量的问答社区和创作者聚集的原创内容平台",
-        link: "https://juejin.cn",
+        icon: "https://static.nowcoder.com/images/logo_web_300.png",
+        title: "牛客",
+        desc: "",
+        link: "https://www.nowcoder.com/",
       },
     ],
   },
@@ -378,22 +579,10 @@ export const NAV_DATA: NavData[] = [
     title: "摸摸鱼",
     items: [
       {
-        icon: "https://www.youtube.com/s/desktop/014dbbed/img/favicon_48x48.png",
-        title: "YouTube",
-        desc: "",
-        link: "https://www.youtube.com",
-      },
-      {
         icon: "https://handle.antfu.me/favicon.svg",
         title: "汉字 - 汉字 Wordle",
         desc: "一个根据拼音猜成语小游戏",
         link: "https://handle.antfu.me/",
-      },
-      {
-        icon: "/icons/twitter.svg",
-        title: "Twitter",
-        desc: "",
-        link: "https://twitter.com",
       },
       {
         icon: "https://momoyu.cc/icon-192.png",
