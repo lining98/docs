20b916563d50c4058894eb558712db0197a340be
lining <ln980908@163.com>
2023-03-10 10:33:48 +0800

    fix bug

--- docs/.vitepress/configs/sidebar.ts
+++ docs/.vitepress/configs/sidebar.ts
@@ -1,9 +1,6 @@
 import type { DefaultTheme } from 'vitepress'
 
 export const sidebar: DefaultTheme.Config['sidebar'] = {
-  "/about/": require("../../about/sidebar"),
-
-
   // "/frontEnd/": require("../../frontEnd/sidebar"),
   "/frontEnd/TypeScript/": require("../../frontEnd/TypeScript/sidebar"),
   "/frontEnd/JavaScript/": require("../../frontEnd/JavaScript/sidebar"),
@@ -16,4 +13,5 @@ export const sidebar: DefaultTheme.Config['sidebar'] = {
   // algorithms
   "/algorithms/": require("../../algorithms/sidebar"),
 
+  "/about/": require("../../about/sidebar"),
 };
