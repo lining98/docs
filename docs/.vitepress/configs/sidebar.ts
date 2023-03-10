14fb9742a9f4a24f419f7db5106a775c8bfd4c11
lining <ln980908@163.com>
2023-03-10 10:14:50 +0800

    feat

--- docs/.vitepress/configs/sidebar.ts
+++ docs/.vitepress/configs/sidebar.ts
@@ -1,19 +1,40 @@
 import type { DefaultTheme } from 'vitepress'
 
+const about: DefaultTheme.Config['sidebar'] = [
+  {
+    text: '关于侧边栏',
+    items: [
+      { text: '关于', link: '/about/' }
+      // { text: "关于2", link: "/about/about2" },
+    ]
+  }
+]
+const frontEnd_Js: DefaultTheme.Config['sidebar'] = [
+  {
+    text: 'JavaScript',
+    items: [{ text: 'JavaScript简介', link: '/frontEnd/JavaScript/' }]
+  }
+]
+const frontEnd_Ts: DefaultTheme.Config['sidebar'] = [
+  {
+    text: 'TypeScript',
+    items: [{ text: 'TypeScript介绍', link: '/frontEnd/TypeScript/' }]
+  }
+]
 export const sidebar: DefaultTheme.Config['sidebar'] = {
-  "/about/": require("../../about/sidebar"),
-
-
   // "/frontEnd/": require("../../frontEnd/sidebar"),
-  "/frontEnd/TypeScript/": require("../../frontEnd/TypeScript/sidebar"),
-  "/frontEnd/JavaScript/": require("../../frontEnd/JavaScript/sidebar"),
+  // "/frontEnd/JavaScript/": require("../../frontEnd/JavaScript/sidebar"),
+  '/frontEnd/JavaScript/': frontEnd_Js,
+  '/frontEnd/TypeScript/': frontEnd_Ts,
   // '/frontEnd/git': require('../../frontEnd/git/sidebar'),
 
   // backEnd
-  "/backEnd/nodejs/": require("../../backEnd/nodejs/sidebar"),
-  "/backEnd/mysql/": require("../../backEnd/mysql/sidebar"),
+  // "/backEnd/nodejs/": require("../../backEnd/nodejs/sidebar"),
+  // "/backEnd/mysql/": require("../../backEnd/mysql/sidebar"),
 
   // algorithms
-  "/algorithms/": require("../../algorithms/sidebar"),
+  // "/algorithms/": require("../../algorithms/sidebar"),
 
-};
+  // "/about/": require("../../about/sidebar"),
+  '/about/': about
+}
