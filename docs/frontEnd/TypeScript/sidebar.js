69ca6e44a26e1cf4a4aef8714c0616f55dd077d2
lining <2357490894@qq.com>
2023-03-11 17:43:50 +0800

    feat:ts

--- docs/frontEnd/TypeScript/sidebar.js
+++ docs/frontEnd/TypeScript/sidebar.js
@@ -2,7 +2,10 @@ module.exports = [
     {
       text: 'TypeScript',
       items: [
-        { text: 'TypeScript介绍', link: '/frontEnd/TypeScript/' },
+        { text: '初识TypeScript', link: '/frontEnd/TypeScript/' },
+        { text: '01_基本类型', link: '/frontEnd/TypeScript/01_基本类型' },
+        { text: '02_对象类型', link: '/frontEnd/TypeScript/02_对象类型' },
+        { text: '03_接口和对象类型', link: '/frontEnd/TypeScript/03_接口和对象类型' },
       ]
     }
   ]
