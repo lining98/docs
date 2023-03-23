8611c51e976aea1d5b96777de51032c792f597ac
lining <2357490894@qq.com>
2023-03-23 23:20:23 +0800

    docs:js closure

--- docs/frontEnd/JavaScript/sidebar.js
+++ docs/frontEnd/JavaScript/sidebar.js
@@ -3,6 +3,7 @@ module.exports = [
       text: 'JavaScript',
       items: [
         { text: 'JavaScript简介', link: '/frontEnd/JavaScript/' },
+        { text: '闭包', link: '/frontEnd/JavaScript/closure' },
       ]
     }
   ]
