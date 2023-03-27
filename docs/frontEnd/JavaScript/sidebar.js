f6bf79f60d7db849fad8ffd7029dd63c23cc5a6a
lining <2357490894@qq.com>
2023-03-27 23:33:28 +0800

    docs:js

--- docs/frontEnd/JavaScript/sidebar.js
+++ docs/frontEnd/JavaScript/sidebar.js
@@ -6,5 +6,16 @@ module.exports = [
         { text: '闭包', link: '/frontEnd/JavaScript/closure' },
         // { text: '作用域', link: '/frontEnd/JavaScript/' },
       ]
+    },
+    {
+      text: '手撕js',
+      items: [
+        { text: '数组去重', link: '/frontEnd/JavaScript/' },
+        { text: '数组扁平化', link: '/frontEnd/JavaScript/' },
+        { text: '节流/防抖', link: '/frontEnd/JavaScript/' },
+        { text: '深拷贝', link: '/frontEnd/JavaScript/' },
+        { text: 'promise', link: '/frontEnd/JavaScript/' },
+        // https://zhuanlan.zhihu.com/p/485338593
+      ]
     }
   ]
