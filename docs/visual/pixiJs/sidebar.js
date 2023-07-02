106296787573bd2edfc161bdf446319c4268b737
lining <2357490894@qq.com>
2023-07-02 14:29:30 +0800

    pixiJs

---
+++ docs/visual/pixiJs/sidebar.js
@@ -0,0 +1,20 @@
+module.exports = [
+  {
+    // text: 'pixiJs',
+    items: [
+      {
+        text: 'pixiJs',
+        items: [
+          { text: '初始pixiJs', link: '/visual/pixiJs/index/初始pixiJs' },
+          { text: '基本使用', link: '/visual/pixiJs/index/基本使用' },
+        ]
+      },
+      {
+        text: '实例',
+        items: [
+          // { text: '梅花特效', link: '/visual/pixiJs/example/梅花特效' },
+        ]
+      }
+    ]
+  }
+]
