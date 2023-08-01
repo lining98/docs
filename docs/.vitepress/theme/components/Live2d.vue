4d276a7ffb83ddcf75339c40e26033512c179df3
lining98 <2357490894@qq.com>
2023-08-01 14:07:52 +0800

    隐藏live2d

--- docs/.vitepress/theme/components/Live2d.vue
+++ docs/.vitepress/theme/components/Live2d.vue
@@ -1,16 +1,16 @@
 <template>
-  <div ref="live2dContentRef"></div>
+  <!-- <div ref="live2dContentRef"></div> -->
 </template>
 
 <script setup lang="ts">
-import { onMounted, ref } from 'vue'
+// import { onMounted, ref } from 'vue'
 
-const live2dContentRef = ref()
-onMounted(async () => {
-  const { Live2d } = await import('@tomiaa/live2d')
-  new Live2d({
-    el: live2dContentRef.value,
-    showLoading: false
-  })
-})
+// const live2dContentRef = ref()
+// onMounted(async () => {
+//   const { Live2d } = await import('@tomiaa/live2d')
+//   new Live2d({
+//     el: live2dContentRef.value,
+//     showLoading: false
+//   })
+// })
 </script>
